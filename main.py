063d2afb282649914666b6abbf9fd567a4da8802
Ayu <y.abouseada16@gmail.com>
2021-04-08 23:51:18 +0200

    v0.2

--- main.py
+++ main.py
@@ -2,3 +2,11 @@
 def say_hello():
     print('hello')
 say_hello()
+#parameters
+def say_hello2(name,emoji):
+    print(f'hello {name} {emoji}')
+# positional arguments
+say_hello2('Juan','*_*')
+
+# keyword arguments
+say_hello2(emoji='*_*',name='John')
