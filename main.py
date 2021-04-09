173634fda132c782fb1a6ea0fc5504f572c36de8
Ayu <y.abouseada16@gmail.com>
2021-04-09 18:39:58 +0200

    v0.4

--- main.py
+++ main.py
@@ -13,7 +13,18 @@ say_hello2(emoji='*_*',name='John')
 
 #return
 
-def sum(num1,num2):
+def uum(num1,num2):
    return num1+num2
 
 # print(sum(int(input('choose a number')),int(input('choose another number'))))
+
+
+#*args **kwargs
+def super_func(*args,**kwargs):
+    print(kwargs)
+    total=0
+    for items in kwargs.values():
+        total += items
+    return sum(args)+total
+
+print(super_func(1,2,3,4,5,6,num5=5,num6=6))
